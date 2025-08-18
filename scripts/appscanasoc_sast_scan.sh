6697f9bcdfb2ef978ccefcc96a161bc4a3da3bdf
jrocia <69405400+jrocia@users.noreply.github.com>
2025-08-18 09:05:36 -0300

    Update appscanasoc_sast_scan.sh

--- scripts/appscanasoc_sast_scan.sh
+++ scripts/appscanasoc_sast_scan.sh
@@ -16,7 +16,7 @@ if ! [ -x "$(command -v appscan.sh)" ]; then
   export PATH="$HOME/SAClientUtil/bin:${PATH}"
 fi
 
-appscan.sh version
+appscan.sh version -acceptssl
 appscan.sh update -acceptssl
 
 # Generate IRX files based on source root folder downloaded by Gitlab
